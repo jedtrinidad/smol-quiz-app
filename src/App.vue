dc2c12d88a96aa018ac03381190f4904bca293fc
Jed Cyril Trinidad <trinidadjedcyril@gmail.com>
2022-02-27 16:25:15 +0800

    create initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-<h1 class="nes-text">SMOL QUIZ APP</h1>
+<h1 class="nes-text">smol quiz app</h1>
+<img class="nes-avatar is-large" src="./assets/logo.png" style="image-rendering: pixelated;">
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
 
 #app {
   text-align: center;
+  margin-top: 1.5em;
 }
 </style>
