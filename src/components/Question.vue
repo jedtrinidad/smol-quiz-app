d33339cc952b50f1ebd4defd55a2e60d739d5296
Jed Cyril Trinidad <trinidadjedcyril@gmail.com>
2022-03-04 23:39:41 +0800

    fetch questions

---
+++ src/components/Question.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  props: ['questionObject']
+}
+</script>
+
+<template>
+  <p v-if="questionObject !== null">{{ questionObject.question }}</p>
+  <p v-else>Waiting for Question...</p>
+</template>
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -3,28 +3,51 @@ import { storeToRefs } from "pinia";
 import { useQuizStore } from "../stores/quiz"
 import API_URL, { endpoints } from '../constants';
 import { ref } from "vue";
+import Question from "./Question.vue";
 
 export default {
   async setup() {
     const quizStore = useQuizStore();
-    const { isPlaying, categoryId } = storeToRefs(quizStore);
+    const { isPlaying } = storeToRefs(quizStore);
 
-    const questions = ref([]);
+    const currentQuestionIndex = ref(1);
+    const currentQuestion = ref(null);
 
-    quizStore.$subscribe((mutation) => {
-      console.log(mutation)
+    const fetchQuestions = async (categoryId, amount) => {
+      try {
+        let response = await fetch(`${API_URL}/${endpoints.QUIZ}?amount=${amount}&category=${categoryId}`);
+        let { results } = await response.json();
+        currentQuestion.value = results[0];
+      }
+      catch (error) {
+        throw error;
+      }
+    };
+
+    quizStore.$subscribe(async (mutation, state) => {
+      if (mutation.storeId === "quiz") {
+        currentQuestion.value = null;
+        if (state.isPlaying === true) {
+          await fetchQuestions(state.categoryId, 10);
+        }
+      }
     });
-
     return {
-      isPlaying
-    }
-  }
+      isPlaying,
+      currentQuestion,
+      currentQuestionIndex
+    };
+  },
+  components: { Question }
 }
 </script>
 
 <template>
   <div class="nes-container with-title" v-if="isPlaying">
-    <p class="title">Question # 1</p>
+    <p class="title">Question # {{ currentQuestionIndex }}</p>
+    <Question
+      :questionObject="currentQuestion"
+    />
   </div>
 </template>
 
