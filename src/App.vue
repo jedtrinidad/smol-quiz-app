1123e9e472167e4ec5049ef0e97aad6fad423b2e
Jed Cyril Trinidad <trinidadjedcyril@gmail.com>
2022-03-17 14:44:32 +0800

    Add score display in page

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@ import { onErrorCaptured, ref } from 'vue';
 import SetupGame from './components/SetupGame.vue';
 import Game from './components/Game.vue';
 import { useQuizStore } from './stores/quiz';
+import { useScoresStore } from './stores/scores';
 import { storeToRefs } from 'pinia';
 
 const quizStore = useQuizStore();
+const scoresStore = useScoresStore();
 const { isPlaying } = storeToRefs(quizStore);
+const { scores } = storeToRefs(scoresStore);
 
 const error = ref(null);
 onErrorCaptured(e => {
@@ -27,6 +30,21 @@ onErrorCaptured(e => {
         </div>
         <div class="container">
           <SetupGame />
+
+          <div class="nes-table-responsive">
+            <table class="nes-table is-centered is-bordered">
+              <thead>
+                <th>Score</th>
+                <th>Time</th>
+              </thead>
+              <tbody>
+                <tr v-for="score in scores">
+                  <td>{{ score.score }}/{{ score.amount }}</td>
+                  <td>{{ score.timestamp }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </template>
@@ -50,6 +68,7 @@ onErrorCaptured(e => {
 
 .container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
